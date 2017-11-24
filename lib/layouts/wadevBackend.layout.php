7df4e3ac5311b26d63ad881792356040279bcbaa
Serge Rodovnichenko <sergerod@gmail.com>
2017-11-24 20:51:12 +0300

    Простой шаблон лэйаута бэкенда

---
+++ lib/layouts/wadevBackend.layout.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @version
+ * @copyright Serge Rodovnichenko, 2017
+ * @license
+ */
+class wadevBackendLayout extends waLayout
+{
+    public function execute()
+    {
+        /**
+         * Include plugins js and css
+         * @event backend_assets
+         * @return array[string]string $return[%plugin_id%]
+         */
+        $this->view->assign('backend_assets', wa()->event('backend_assets'));
+
+        $this->view->assign(array(
+            'is_debug' => (bool) waSystemConfig::isDebug(),
+        ));
+    }
+}
