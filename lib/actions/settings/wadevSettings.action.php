76c0970278bdad1d69da2c0bdbba620a73bf3ccd
Serge Rodovnichenko <sergerod@gmail.com>
2017-11-25 00:06:21 +0300

    Экшен показаэкрана настроек

---
+++ lib/actions/settings/wadevSettings.action.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @copyright Serge Rodovnichenko, 2017
+ */
+class wadevSettingsAction extends wadevViewAction
+{
+    public function execute()
+    {
+        //@todo: checkRights
+    }
+}
