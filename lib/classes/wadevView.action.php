0694cff04cc632f0a9409ab2646d187592f2be92
Serge Rodovnichenko <sergerod@gmail.com>
2017-11-24 21:06:03 +0300

    Базовый класс для viewAction приложения

---
+++ lib/classes/wadevView.action.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @version
+ * @copyright Serge Rodovnichenko, 2017
+ * @license
+ */
+class wadevViewAction extends waViewAction
+{
+    public function __construct($params = null)
+    {
+        parent::__construct($params);
+        if (!wadevHelper::isAjax()) {
+            $this->setLayout(new wadevBackendLayout());
+        }
+    }
+}
