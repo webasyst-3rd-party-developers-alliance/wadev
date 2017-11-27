bdcea547f1ff356e86b85a1f43b708e6fb4a031f
Kirill Maramygin <kirill.maramygin@gmail.com>
2017-11-27 11:21:22 +0300

    fix app icon count

--- lib/config/wadevConfig.class.php
+++ lib/config/wadevConfig.class.php
@@ -112,10 +112,10 @@ class wadevConfig extends waAppConfig
 
     public function onCount()
     {
-        $new_transactions = 0;
+        $new_transactions = (int) $this->getSetting('new_transactions');
         $transaction = new wadevTransaction();
         if ($new_ones = $transaction->updateFromApi()) {
-            $new_transactions = (int) $this->getSetting('new_transactions') + $new_ones;
+            $new_transactions += $new_ones;
             $this->setSetting('new_transactions', $new_transactions);
         }
         return $new_transactions;
