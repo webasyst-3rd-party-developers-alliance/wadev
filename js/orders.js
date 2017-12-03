0aad4fb244a28ad8ffe0b1ea7cc7f12a4994a06b
Serge Rodovnichenko <sergerod@gmail.com>
2017-12-04 02:03:22 +0300

    Теперь окно показывает спиннер для пока заказ загружается

--- js/orders.js
+++ js/orders.js
@@ -11,8 +11,17 @@ var OrderDialog = (function ($) {
             data['order_id'] = order_id;
         }
 
-        $.get(href, data, function (html) {
-            new WadevDialog({html: html})
+        if(that.xhr) {
+            that.xhr.abort();
+            that.xhr = false;
+        }
+
+        var dialog_wrapper = '<div class="w-dialog-wrapper is-full-screen w-view-order-dialog" id="w-view-order"><div class="w-dialog-background"></div><div class="w-dialog-block gray-header compact-header"><div class="loading128"></div></div></div>';
+        dialog = new WadevDialog({html: dialog_wrapper});
+        that.xhr = $.get(href, data, function (html) {
+            $('.w-dialog-block', '#w-view-order').html(html);
+            dialog.setPosition();
+            //new WadevDialog({html: html})
         });
     };
 
