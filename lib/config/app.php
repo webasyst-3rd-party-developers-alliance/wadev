edfa63fe19cb2cdbefcc145812f75276b2ccb7b8
Serge Rodovnichenko <sergerod@gmail.com>
2018-07-24 23:16:12 +0300

    version up

--- lib/config/app.php
+++ lib/config/app.php
@@ -6,6 +6,6 @@ return array(
             48 => 'img/wadev48.png',
             96 => 'img/wadev96.png',
         ),
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'vendor'  => '670917',
 );
