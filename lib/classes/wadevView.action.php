f74d75c782c3b1aba0170e60f9e2aa258792f991
Serge Rodovnichenko <sergerod@gmail.com>
2019-12-01 03:41:55 +0300

    Пометил базовый класс как абстрактный

--- lib/classes/wadevView.action.php
+++ lib/classes/wadevView.action.php
@@ -2,11 +2,9 @@
 
 /**
  * @author Serge Rodovnichenko <serge@syrnik.com>
- * @version
  * @copyright Serge Rodovnichenko, 2017
- * @license
  */
-class wadevViewAction extends waViewAction
+abstract class wadevViewAction extends waViewAction
 {
     public function __construct($params = null)
     {
